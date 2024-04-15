ed7852997ab4d32b81ee6d1b72cd0c8f7dfb2637
Caleb Brent Voskuil <caleb.voskuil13@gmail.com>
2024-04-15 18:34:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyIngredients worker = new MyIngredients();
+            worker.Run();
+        }
+    }
+}
