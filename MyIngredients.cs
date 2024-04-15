6f376a6fc96d7b0b298bbcee64de63ebbb6d8836
Caleb Brent Voskuil <caleb.voskuil13@gmail.com>
2024-04-15 19:02:55 +0200

    added the basic layout of my main function and the functions it will call

--- MyIngredients.cs
+++ MyIngredients.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace MyRecipeApp
 {
-    internal class MyIngredients
+    class MyIngredients
     {
-        public void Run()
-        {
-            Console.WriteLine("My Ingredients");
-        }
+
     }
+        
   
 }
--- Program.cs
+++ Program.cs
@@ -6,12 +6,55 @@ using System.Threading.Tasks;
 
 namespace MyRecipeApp
 {
+    /// <summary>
+    /// Caleb Voskuil
+    /// ST10397320
+    /// Prog6221
+    /// group 1
+    /// References:
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
             MyIngredients worker = new MyIngredients();
-            worker.Run();
+            while (true)
+            {
+                Console.WriteLine("1. Enter Recipe Details");
+                Console.WriteLine("2. Display Recipe");
+                Console.WriteLine("3. Scale Recipe");
+                Console.WriteLine("4. Reset");
+                Console.WriteLine("5. Exit");
+
+                Console.WriteLine("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        worker.EnterRecipeDetails();
+                        break;
+                    case 2:
+                        worker.DisplayRecipe();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the scale factor: ");
+                        float factor = float.Parse(Console.ReadLine());
+                        worker.ScaleRecipe();
+                        break;
+                    case 4:
+                        worker.Reset();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+            
         }
     }
 }
+//-----------------------------------------------O________________END_OF_FILE________________O----------------------------------------------//
