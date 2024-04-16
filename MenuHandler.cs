8f044a69471716352d2b0bbf2f30eba5fb52474f
Caleb Voskuil <ST10397320@vcconnect.edu.za>
2024-04-16 02:29:03 -0700

    added a menu handler class to clean up my main method

---
+++ MenuHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeApp
+{
+    internal class MenuHandler
+    {
+        private MyIngredients worker = new MyIngredients();
+        public void HandleMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n1. Enter Recipe Details");
+                Console.WriteLine("2. Display Recipe");
+                Console.WriteLine("3. Scale Recipe");
+                Console.WriteLine("4. Reset");
+                Console.WriteLine("5. Exit");
+
+                Console.WriteLine("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+
+                switch (choice)
+                {
+                    case 1:
+                        worker.EnterRecipeDetails();
+                        break;
+                    case 2:
+                        worker.DisplayRecipe();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the scale factor: ");
+                        float factor = float.Parse(Console.ReadLine());
+                        worker.ScaleRecipe(factor);
+                        break;
+                    case 4:
+                        worker.Reset();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -17,45 +17,11 @@ namespace MyRecipeApp
     {
         static void Main(string[] args)
         {
-            MyIngredients worker = new MyIngredients();
-            while (true)
-            {
-                Console.WriteLine("\n1. Enter Recipe Details");
-                Console.WriteLine("2. Display Recipe");
-                Console.WriteLine("3. Scale Recipe");
-                Console.WriteLine("4. Reset");
-                Console.WriteLine("5. Exit");
+            MenuHandler menu = new MenuHandler();
+            menu.HandleMenu();
 
-                Console.WriteLine("Enter your choice: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("\n");
-
-                switch(choice)
-                {
-                    case 1:
-                        worker.EnterRecipeDetails();
-                        break;
-                    case 2:
-                        worker.DisplayRecipe();
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter the scale factor: ");
-                        float factor = float.Parse(Console.ReadLine());
-                        worker.ScaleRecipe(factor);
-                        break;
-                    case 4:
-                        worker.Reset();
-                        break;
-                    case 5:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice");
-                        break;
-                }
-            }
-            
         }
+
     }
 }
 //-----------------------------------------------O________________END_OF_FILE________________O----------------------------------------------//
